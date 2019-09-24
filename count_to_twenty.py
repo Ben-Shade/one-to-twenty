baac8ec17bc1bd3c4b0dc2c359123edd3ceaf5be
Ben-Shade <55599909+Ben-Shade@users.noreply.github.com>
2019-09-24 14:11:26 -0500

    Add files via upload

---
+++ count_to_twenty.py
@@ -0,0 +1,13 @@
+#ben shade
+#Counts_to_twenty this program count to 20
+
+print("this counts to 20")
+
+number = 0
+
+while number < 21:
+    print(number , end = " " )
+    number += 1
+    
+
+   
